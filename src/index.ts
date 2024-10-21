78c68f44e39ff704afa35744c1874f95b42f6857
Vladislav Shumkin <vldslv.shumkin@gmail.com>
2024-10-21 21:03:12 +0500

    added http server

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,19 @@
-// import TelegramBot, { Message } from 'node-telegram-bot-api'
-// import {  } from 'tesseract.js'
-import 'dotenv/config'
-// import { createReadStream, readFileSync } from 'fs'
-
-const token = process.env.TELEGRAM_BOT_TOKEN;
-// const bot = new TelegramBot(token, { polling: true });
-
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id;
-//   const messageText = msg.text;
-
-//   if (messageText === '/start') {
-//     bot.sendMessage(chatId, 'Отправьте накладную');
-//   }
-// });
-
-// const onPhotoHandler = async (msg: Message) => {
-//   const chatId = msg.chat.id;
-//   const document = msg.document;
-//   const photo = msg.photo?.[0];
-//   const fileId = photo?.file_id || document?.file_id;
-//   if (!fileId) {
-//     bot.sendMessage(chatId, 'Ошибка при получении Id файла. Попробуйте отправить фото еще раз');
-//     return;
-//   }
-//   const file = await bot.getFile(fileId);
-
-//   console.log(file);
-
-//   bot.sendMessage(chatId, 'Фотография получена');
-//   if (file.file_path) {
-//     bot.sendDocument(chatId, 'https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?cs=srgb&dl=pexels-soldiervip-1308881.jpg&fm=jpg');
-//     bot.sendPhoto(chatId, 'https://telegram.org/img/t_logo.png');
-//   }
-// }
-
-// bot.on('photo', onPhotoHandler)
-// bot.on('document', onPhotoHandler)
-
-
-
+import { createServer } from 'http'
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
+import 'dotenv/config'
+
 import { recognizeText } from './services/recognizeText.js';
 
+
+const server = createServer((req, res) => {
+  res.end('Hello World')
+})
+server.listen(3333)
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new Telegraf(token)
+
 bot.start((ctx) => ctx.reply('Welcome', {
   reply_markup: {
     inline_keyboard: [
