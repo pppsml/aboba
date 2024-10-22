4dfab31cd4c535c3cac98be55e96e4ce2407483c
Vladislav Shumkin <vldslv.shumkin@gmail.com>
2024-10-22 22:23:07 +0500

    moved from telegraf to node-telegram-bot-api, fixed tesseract recognition

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { createServer } from 'http'
-import { Telegraf } from 'telegraf'
-import { message } from 'telegraf/filters'
+import TelegramBot, {  } from 'node-telegram-bot-api'
 import 'dotenv/config'
 
 import { recognizeText } from './services/recognizeText.js';
-
 
 const server = createServer((req, res) => {
   res.end('Hello World')
@@ -12,37 +10,30 @@ const server = createServer((req, res) => {
 server.listen(3333)
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new Telegraf(token)
+const bot = new TelegramBot(token, { polling: true })
 
-bot.start((ctx) => ctx.reply('Welcome', {
-  reply_markup: {
-    inline_keyboard: [
-      [{ text: 'oldschool', callback_data: 'oldschool' }],
-      [{ text: 'hipster', callback_data: 'hipster' }],
-    ],
-  },
-}))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('hipster', Telegraf.reply('λ'))
-
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.on(message('document'), (ctx) => {
-  const photo = ctx.message.document
-  if (!photo) return ctx.reply('Photo not found')
-  const fileId = photo.file_id
-  ctx.reply('Photo found')
-  bot.telegram.getFileLink(fileId).then(async (link) => {
+bot.on('document', async (msg) => { 
+  const document = msg.document
+  if (!document) return
+  const fileId = document.file_id
+  const { message_id } = await bot.sendMessage(msg.chat.id, 'Processing...', { reply_to_message_id: msg.message_id })
+  bot.getFileLink(fileId).then(async (link) => {
     // console.log(fileId, link)
     const text = await recognizeText(link.toString())
-    ctx.reply(text)
+    bot.editMessageText(text, { message_id, chat_id: msg.chat.id })
   })
+  // const photo = ctx.message.document
+  // if (!photo) return ctx.reply('Photo not found')
+  // const fileId = photo.file_id
+  // const { message_id } = await ctx.reply('Processing...', { reply_parameters: { message_id: ctx.message.message_id } })
+  // bot.telegram.getFileLink(fileId).then(async (link) => {
+  //   // console.log(fileId, link)
+  //   const text = await recognizeText(link.toString())
+  //   ctx.edit
+  // })
 })
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
 
-bot.on('document', async (ctx) => ctx.reply('Document received'))
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+// process.once('SIGINT', () => bot.stop('SIGINT'))
+// process.once('SIGTERM', () => bot.stop('SIGTERM'))
--- src/services/recognizeText.ts
+++ src/services/recognizeText.ts
@@ -1,8 +1,7 @@
-import { createWorker } from 'tesseract.js'
-
+import Tesseract, { createWorker } from 'tesseract.js'
 
 export const recognizeText = async (image: any) => {
-  const worker = await createWorker('rus')
+  const worker = await createWorker(['rus', 'eng'], Tesseract.OEM.DEFAULT, { cacheMethod: 'none' })
   const ret = await worker.recognize(image)
   await worker.terminate()
   return ret.data.text
